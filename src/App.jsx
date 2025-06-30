f8e9e5923e83471454ade2603489b18495a66af8
mostafaezz13 <mostafaezz133@gmail.com>
2025-06-30 13:13:16 +0300

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Home from './components/home/Home'
-import About from './components/about/About'
-import Projects from './components/projects/Projects'
-import Skills from './components/skills/Skills'
-import Services from './components/services/Services'
-import Nav from './components/nav/Nav'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import Home from './components/home/Home.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Skills from './components/skills/Skills.jsx'
+import Services from './components/services/Services.jsx'
+import Projects from './components/projects/Projects.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
 function App() {
   return (
     <>
